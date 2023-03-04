18c85b53621d9d43372aefe177adca989aebac87
Jason Aricheta <jason.aricheta@gmail.com>
2023-03-05 12:23:06 +1300

    added form and validation to client creation

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,8 @@ class Client < ApplicationRecord
   # note plural here even if model is singular 
   # - also, associated invoices are destroyed when client is destroyed
   has_many :invoices, dependent: :destroy
+
+  # validations
+  # email required and unique (note on app-level enforcement, not db)
+  validates :email, presence: true, uniqueness: true
 end
