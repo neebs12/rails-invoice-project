a278583f2258f56940a1a75aefdf18aa323a1dd6
Jason Aricheta <jason@hnry.io>
2023-03-07 05:44:33 +1300

    added nesting for invoice and invoice items

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # make CRUD paths available in addition to path helpers
-  resources :invoices
+  resources :invoices do
+    # nested resource for invoice_items
+    resources :invoice_items
+  end
   # make only the new and create paths & helpers available for clients
   resources :clients, only: [:new, :create]
 
