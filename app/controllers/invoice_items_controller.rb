7e7d45ebde2818a7bde9e8772007e564a8b4c344
Jason Aricheta <jason@hnry.io>
2023-03-07 12:51:34 +1300

    functional InvoiceItems#create action

--- app/controllers/invoice_items_controller.rb
+++ app/controllers/invoice_items_controller.rb
@@ -8,7 +8,22 @@ class InvoiceItemsController < ApplicationController
   end
 
   def create
+    # this is a POST invoice/:invoice_id/invoice_items request
+    p params
+    @invoice_item = InvoiceItem.new(invoice_item_params)
+    # assign the fk invoice_id to invoice_item for completion - fyi, is mutation
+    @invoice_item.invoice_id = params[:invoice_id]
+    if @invoice_item.save
+      # redirect to the invoice page - but the one that creates new invoice items!!
+      redirect_to new_invoice_invoice_item_path(params[:invoice_id])
+    else
+      # render the new page again
+      # render :new, status: :unprocessable_entity
+      p ">>> #{params}"
+      p ">>> error, unable to save invoice item"
+    end
   end
+
   private
   def invoice_item_params
     params.require(:invoice_item).permit(:service, :quantity, :price)
