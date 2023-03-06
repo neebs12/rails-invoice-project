ac8f5c6a2ddcdf81fa635966dcdd6cc245fff518
Jason Aricheta <jason@hnry.io>
2023-03-07 06:25:27 +1300

    added new and create to invoice items controller

--- app/controllers/invoice_items_controller.rb
+++ app/controllers/invoice_items_controller.rb
@@ -1,2 +1,12 @@
 class InvoiceItemsController < ApplicationController
+  def new 
+    p "render new!!!!!!!!!! - this is invoice items"
+    # this receives a GET invoice/:invoice_id/invoice_items request
+    # @invoice_item = InvoiceItem.new
+    # for rendering the current state of the invoice
+    @invoice = Invoice.includes(:invoice_items, :client).find(params[:invoice_id])
+  end
+
+  def create
+  end
 end
