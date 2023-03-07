a493041c5b8c77dc42e4c9f85935f9e796e3c1b8
Jason Aricheta <jason@hnry.io>
2023-03-08 08:40:40 +1300

    added invoices index view and controller

---
+++ app/controllers/invoices_controller.rb
@@ -0,0 +1,8 @@
+class InvoicesController < ApplicationController
+  # both root "invoices#index" and root "articles#index" will map to this controller action
+  def index
+    # eager loading, essentially a join.
+    @invoices = Invoice.includes(:invoice_items ,:client).all
+  end
+
+end
