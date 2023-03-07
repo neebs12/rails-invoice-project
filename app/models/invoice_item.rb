cb8e6869f5e0d87f70bb016eb5cbd94eaf55226d
Jason Aricheta <jason@hnry.io>
2023-03-07 14:30:45 +1300

    added validation to invoice item model

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,3 +1,7 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :invoice
+
+  validates :service, presence: true
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than: 0 }, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
 end
