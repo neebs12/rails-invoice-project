6f0b7757be5decf756ea655035c5704123042e42
Jason Aricheta <jason@hnry.io>
2023-03-08 16:20:04 +1300

    added invoices path and route path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Defines the root path for the application
+  root "invoices#index"
+  # make CRUD paths available in addition to path helpers
+  resources :invoices
 end
