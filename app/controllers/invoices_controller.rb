84cfe75f95901561c3507f4012cb4b1783f7c0f4
Jason Aricheta <jason@hnry.io>
2023-03-10 15:17:42 +1300

    added totals calculation to the controller
    - kept DRY by addition of a private method to eval @totals and @total.
    - these @ variables will be used in the view

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -3,9 +3,26 @@ class InvoicesController < ApplicationController
   def index
     # eager loading, essentially a join.
     @invoices = Invoice.includes(:invoice_items ,:client).all
+    # total calculation, done via accompanying total value. Mapped to the invoice object.
+    @totals = {}
+    @invoices.each do |invoice|
+      @totals[invoice] = calculate_total(invoice)
+    end
   end
 
   def show
     @invoice = Invoice.includes(:invoice_items, :client).find(params[:id])
+    @total = calculate_total(@invoice)
+  end
+
+  private
+  def calculate_total(invoice)
+    total = 0
+    invoice.invoice_items.each do |item|
+      quantity = item.quantity
+      price = item.price
+      total += (quantity * price)
+    end
+    total
   end
 end
