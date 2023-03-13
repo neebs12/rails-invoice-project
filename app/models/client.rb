33927801f89323712a8e515a8b72d4c459bd5f78
Jason Aricheta <jason@hnry.io>
2023-03-14 10:49:18 +1300

    added model asscs between client invoice and user

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,5 @@
 class Client < ApplicationRecord
+  belongs_to :user
   # note plural here even if model is singular 
   # - also, associated invoices are destroyed when client is destroyed
   has_many :invoices, dependent: :destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # associations
+  has_many :clients, dependent: :destroy
+  has_many :invoices, dependent: :destroy
 end
