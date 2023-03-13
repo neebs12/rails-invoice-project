0c93dc46236c2a077ad6b44fa1f00228ea1dd0fd
Jason Aricheta <jason@hnry.io>
2023-03-14 10:50:26 +1300

    proper invoice items should now appear on signin

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -4,7 +4,7 @@ class InvoicesController < ApplicationController
   # both root "invoices#index" and root "articles#index" will map to this controller action
   def index
     # eager loading, essentially a join.
-    @invoices = Invoice.includes(:invoice_items ,:client).all
+    @invoices = Invoice.includes(:invoice_items ,:client).where(user: current_user)
   end
 
   def show
