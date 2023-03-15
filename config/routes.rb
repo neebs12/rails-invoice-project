3748f5fcc56e68ecbb2ca840d9b7b0f17e714f15
Jason Aricheta <jason@hnry.io>
2023-03-16 11:17:04 +1300

    added a post route to the controller action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path for the application
   root "invoices#index"
+
+  # Defines the path for the send_email action
+  post "/send_email", to: "invoices#send_email"
+
   # make CRUD paths available in addition to path helpers
   resources :invoices
 end
