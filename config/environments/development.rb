ed661727164f4fbbd59a4c7b7a497373d1ee5d08
Jason Aricheta <jason@hnry.io>
2023-03-16 11:13:09 +1300

    added smtp settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,16 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:         'smtp.gmail.com',
+    port:            587,
+    domain:          'example.com',
+    user_name:       ENV['GMAIL_EMAIL'],
+    password:        ENV['GMAIL_APP_PASS'],
+    authentication:  'plain',
+    enable_starttls: true,
+    open_timeout:    5,
+    read_timeout:    5 
+  }    
 end
