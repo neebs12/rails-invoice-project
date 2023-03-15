d02964d36e3bc2bf04ef92676a3ac1938ffae317
Jason Aricheta <jason@hnry.io>
2023-03-16 11:14:40 +1300

    added user mailer functionality

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,7 @@
+class UserMailer < ApplicationMailer
+  def send_email
+    @email = params[:email]
+    @invoice_id = params[:invoice_id]
+    mail(to: @email, subject: 'Hello! from the Rails APP!')
+  end  
+end
