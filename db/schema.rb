98e8bebaa4cea6f72a852bd4a1fe6bb9d5ba7d34
Jason Aricheta <jason@hnry.io>
2023-03-29 09:11:39 +1300

    modified schema for user, invoice & client assc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_27_001828) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_28_195834) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_001828) do
     t.string "name"
     t.string "phone"
     t.string "address"
+    t.integer "user_id"
   end
 
   create_table "invoice_items", force: :cascade do |t|
@@ -37,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_001828) do
     t.bigint "client_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["client_id"], name: "index_invoices_on_client_id"
   end
 
@@ -52,6 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_001828) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "clients", "users"
   add_foreign_key "invoice_items", "invoices", on_delete: :cascade
   add_foreign_key "invoices", "clients", on_delete: :cascade
+  add_foreign_key "invoices", "users"
 end
