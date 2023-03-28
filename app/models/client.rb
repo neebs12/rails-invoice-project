867af3b27cf7057a01ac095d1f59adacf61777b2
Jason Aricheta <jason@hnry.io>
2023-03-29 09:15:09 +1300

    linked users, invoices and clients models

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,6 @@
 class Client < ApplicationRecord
+  belongs_to :user
+
   # note plural here even if model is singular
   # - also, associated invoices are destroyed when client is destroyed
   has_many :invoices, dependent: :destroy
