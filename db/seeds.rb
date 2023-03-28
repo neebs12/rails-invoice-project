3271d20504925942bc8e7ccd678b0afa06b88af7
Jason Aricheta <jason@hnry.io>
2023-03-29 09:15:38 +1300

    modified seeds to reflect new associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 # erase all clients (deletetion should cascade to invoices and invoice_items)
+User.destroy_all
 Client.destroy_all
 
 # Create some users
@@ -14,13 +15,13 @@ user1 = User.create(email: 'jason@yahoo.com', password: 'seedpassword')
 user2 = User.create(email: 'jason@gmail.com', password: 'seedpassword2')
 
 # Create some clients
-client1 = Client.create(email: 'beher80339@kaudat.com', name: 'Forman L. Smith', phone: '555-555-5555', address: '123 Main St.')
-client2 = Client.create(email: 'bar@gmail.com', name: 'Bar L. Low', phone: '666-666-6665', address: '430 Omapere Boulevard.')
-client3 = Client.create(email: 'baz@ymail.com', name: 'Baz McKensy', phone: '777-777-7775', address: '700 Besttest Av.')
+client1 = Client.create(user: user1, email: 'beher80339@kaudat.com', name: 'Forman L. Smith', phone: '555-555-5555', address: '123 Main St.')
+client2 = Client.create(user: user2, email: 'bar@gmail.com', name: 'Bar L. Low', phone: '666-666-6665', address: '430 Omapere Boulevard.')
+client3 = Client.create(user: user2, email: 'baz@ymail.com', name: 'Baz McKensy', phone: '777-777-7775', address: '700 Besttest Av.')
 
 # Create some invoices for client1
-invoice1 = Invoice.create(client: client1)
-invoice2 = Invoice.create(client: client1)
+invoice1 = Invoice.create(user: user1, client: client1)
+invoice2 = Invoice.create(user: user1, client: client1)
 
 # Create some invoice item(s) for invoice1
 InvoiceItem.create(invoice: invoice1, service: 'Consulting-foo', quantity: 7, price: 100.0)
@@ -30,9 +31,9 @@ InvoiceItem.create(invoice: invoice1, service: 'Training-foo', quantity: 1, pric
 InvoiceItem.create(invoice: invoice2, service: 'Development-foo', quantity: 5, price: 200.0)
 
 # Create some invoices for client2
-invoice3 = Invoice.create(client: client2)
-invoice4 = Invoice.create(client: client2)
-invoice5 = Invoice.create(client: client2)
+invoice3 = Invoice.create(user: user2, client: client2)
+invoice4 = Invoice.create(user: user2, client: client2)
+invoice5 = Invoice.create(user: user2, client: client2)
 
 # Create some invoice items for invoice3
 InvoiceItem.create(invoice: invoice3, service: 'Design', quantity: 8, price: 500.0)
@@ -48,7 +49,7 @@ InvoiceItem.create(invoice: invoice5, service: 'CDKtf', quantity: 7, price: 900.
 InvoiceItem.create(invoice: invoice5, service: 'Actions_Yumml', quantity: 1, price: 1000.01)
 
 # Create some invoices for client3
-invoice6 = Invoice.create(client: client3)
+invoice6 = Invoice.create(user: user2, client: client3)
 
 # Create some invoice items for invoice6
 InvoiceItem.create(invoice: invoice6, service: 'Consulting', quantity: 1, price: 1000.0)
